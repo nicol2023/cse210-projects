0d95ef490475909b7e8755add355687de0fa9511
Nicol <nicol_lds@hotmail.com>
2024-05-04 17:05:39 -0300

    finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        int guess;
+        string feedback = "";
+
+        while (true)
+        {
+            Console.WriteLine("What's your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < number)
+            {
+            feedback = "Higher";
+            }
+            else if (guess > number)
+            {
+            feedback = "Lower";
+            }
+            else
+            {
+            feedback = "You guessed it!";
+            break;
+            }
+
+            Console.WriteLine(feedback);
+        }
+
+        Console.WriteLine(feedback);
     }
 }
