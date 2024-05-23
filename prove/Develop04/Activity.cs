8660cb2a656633c22bedf159ff0077b6b2e7a07e
Nicol <nicol_lds@hotmail.com>
2024-05-23 20:54:10 -0300

    finally

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Threading;
+
+public class Activity 
+{
+    protected string _name;
+    protected string _description;
+    protected int _duration;
+
+    public Activity()
+    {
+
+    }
+
+    public void DisplayStartingMessage()
+    {
+        Console.WriteLine($"Starting {_name} activity.");
+        Console.WriteLine(_description);
+        Console.WriteLine("How many seconds would you like for this activity? ");
+        _duration = int.Parse(Console.ReadLine()); // Convertir la entrada a entero
+        Console.WriteLine("Prepare to begin...");
+        ShowSpinner(3);
+    }
+
+    public void DisplayEndingMessage()
+    {
+        Console.WriteLine("Well done!");
+        Console.WriteLine($"You have completed the {_name} activity for {_duration} seconds.");
+        ShowSpinner(3);
+    }
+
+    public void ShowSpinner(int seconds)
+    {
+        for (int i = 0; i < seconds; i++)
+        {
+            Console.Write("|");
+            Thread.Sleep(250);
+            Console.Write("\b/");
+            Thread.Sleep(250);
+            Console.Write("\b-");
+            Thread.Sleep(250);
+            Console.Write("\b\\");
+            Thread.Sleep(250);
+            Console.Write("\b");
+        }
+    }
+
+    public void ShowCountDown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+    }
+}
